b31b6cca95a51f9bb80a398902e3b51a7379f8f4
19b709-HY <s19b709@shohoku.ac.jp>
2020-10-02 12:14:25 +0900

    絵が表示されるよう改善しました

--- app/src/main/java/jp/ac/shohokus19b702/graphicssample/CustomView2.java
+++ app/src/main/java/jp/ac/shohokus19b702/graphicssample/CustomView2.java
@@ -1,8 +1,14 @@
 package jp.ac.shohokus19b702.graphicssample;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
+
 
 import androidx.annotation.Nullable;
 
@@ -18,4 +24,26 @@ public class CustomView2 extends View {
     public CustomView2(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
+
+    protected void onDraw(Canvas canvas){
+        super.onDraw(canvas);
+        canvas.drawColor(Color.WHITE);
+
+        int w = this.getWidth();
+        int h = this.getHeight();
+
+        Paint p = new Paint();
+        p.setStyle(Style.STROKE); //Style を設定: 線で描く
+        p.setColor(Color.DKGRAY); //色を設定
+        canvas.drawRect(new Rect(5, 5, w - 10, h - 10), p); //長方形を描画
+
+        for(int i = 0; i<10; i++){
+            Paint p2 = new Paint();
+            p2.setStyle(Style.FILL); //Style を設定： 塗りつぶす
+            p2.setColor(Color.rgb(25*i, 0, 0)); //色を設定
+            canvas.drawCircle(25*i+125, 25*i+125, 100,p2); //円を描く
+        }
+    }
+
+
 }
